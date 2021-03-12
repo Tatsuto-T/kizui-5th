55d7c7bfb8dfeefc2b5c8639d3d83fa7b19ef990
“Tatsuto <tatsutott9@gmail.com>
2021-03-12 17:46:55 +0900

    残りはハンバーガーの外クリックでなくなるやつ

--- js/script.js
+++ js/script.js
@@ -11,9 +11,10 @@
 // });
 
 
-var scroll = new SmoothScroll('a[href*="#"]', {
-    header: '#header'
-});
+
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        header: '#header'
+    });
 // スティッキウーヘッダー-----------------------------------
 $(function (){
     var $win = $(window)
@@ -22,18 +23,73 @@ $(function (){
     var fvHeight = $fv.outerHeight();
     var fixedClass = "fixed";
 
+        // $win.on("load scroll", function(){
+        //     var value = $(this).scrollTop();
+        //     if($win.width() > 768){
+        //         if( value > fvHeight){
+        //             $header.addClass(fixedClass);
+        //         } else {
+        //             $header.removeClass(fixedClass);
+        //         }
+        //     }
+        // })
+
         $win.on("load scroll", function(){
             var value = $(this).scrollTop();
-            if($win.width() > 768){
-                if( value > fvHeight){
+                if( value >= fvHeight){
                     $header.addClass(fixedClass);
                 } else {
                     $header.removeClass(fixedClass);
                 }
-            }
         })
 })
 
 // AOS。JSーーーーーーーーーーーーーーーーーーーーーーーー
 AOS.init()
 // 2
+
+// バーガーボタン-----------------------------------
+$(function(){
+    $(".burger-btn").on("click",function() {
+        $(".burger-btn").toggleClass("close");
+        $(".bglayer").toggleClass("open");
+        $(".header-nav-res").toggleClass("active");
+        // $(".header").css('color','red');
+    })
+
+    var $resitem = $(".header-nav-res-list-items");
+
+    $resitem.on("click",function() {
+        $(".burger-btn").removeClass("close");
+        $(".bglayer").removeClass("open");
+        $(".header-nav-res").removeClass("active");
+    })
+
+    if($(".header-nav-res").hasClass("active")){
+
+    }
+
+    $("main").on('click',function() {
+        $(".burger-btn").removeClass("close");
+        $(".bglayer").removeClass("open");
+        $(".header-nav-res").removeClass("active");
+    })
+});
+
+// $(document).click(function() {　$('.header-nav-res').hide();　});
+
+// $(document).click(function(event) {
+//     if(!$(event.target).closest('.header-nav-res').length) {
+//         $(".burger-btn").removeClass("close");
+//         $(".bglayer").removeClass("open");
+//         $(".header-nav-res").removeClass("active");
+//         console.log('外側がクリックされました。');
+//     } else {                console.log('内側がクリックされました。');
+//     }
+
+// });
+
+
+
+
+
